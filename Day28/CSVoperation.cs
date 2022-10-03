9a206b2441b7c11c44202bce4e6b28588cfcc828
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-10-03 17:05:25 +0530

    [Avhi]Added:Ability to read & write into csv fie

---
+++ Day28/CSVoperation.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Day28
+{
+    public class CSVoperation
+    {
+        public static void CsvSerialize()
+        {
+            Console.WriteLine("WelCome");
+            Student s = new Student();
+
+            string csvFilePath = @"C:\Users\Administrator\Desktop\Day28_CSV-JASON\Day28\Data.csv";
+
+            List<Student> students = new List<Student>()
+            {
+                new Student(){FName="Abhi",LName="Bhave",Address="Pune",ZipCode=411028},
+                new Student(){FName="Rahul",LName="walanj",Address="Pune",ZipCode=411028}
+            };
+
+            StreamWriter sw = new StreamWriter(csvFilePath);
+            CsvWriter cw = new CsvWriter(sw, CultureInfo.InvariantCulture);
+
+            cw.WriteRecords<Student>(students);
+            sw.Close();
+            Console.WriteLine("Closed");
+        }
+
+        public static void CsvDeserialize()
+        {
+            string csvFilePath = @"C:\Users\Administrator\Desktop\Day28_CSV-JASON\Day28\Data.csv";
+            StreamReader swReader = new StreamReader(csvFilePath);
+
+            CsvReader csvReader = new CsvReader(swReader, CultureInfo.InvariantCulture);
+
+            List<Student> students1 = csvReader.GetRecords<Student>().ToList();
+
+            foreach (Student student in students1)
+            {
+                Console.WriteLine(student);
+            }
+            
+
+           
+        }
+    }
+}
---
+++ Day28/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day28
+{
+    public class Student
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FName} {LName} {Address} {ZipCode}";
+        }
+    }
+}
